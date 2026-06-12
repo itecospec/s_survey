975d6425143422964b47e2205223301666a58a6e
itecospec <it@ecospec.ie>
2026-06-12 08:52:02 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@ const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icons/icon-384.png',
-  './icons/icon-384.png',
+  'icon-384.png',
+  'icon-384.png',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
